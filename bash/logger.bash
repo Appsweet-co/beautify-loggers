57dd0fe6538b7375470de17d90d30eb86d3cdb67
dperuo <derek@derekperuo.net>
2021-10-29 12:52:56 -0500

    feat: (logger.bash) Set emoji as standard, no customization

--- bash/logger.bash
+++ bash/logger.bash
@@ -13,43 +13,43 @@ RESET="\e[0m"
 
 ########
 
-MSG_DEBUG="${MAGENTA}==> [DEBUG]"
-MSG_DOCS="${CYAN}==> [DOCS]"
-MSG_ERROR="${RED}==> [ERROR]"
-MSG_INFO="${BLUE}==> [INFO]"
-MSG_OK="${GREEN}==> [OK]"
-MSG_WARN="${YELLOW}==> [WARN]"
+MSG_DEBUG="${MAGENTA}[DEBUG] 👾"
+MSG_DOCS="${CYAN}[DOCS] 📝"
+MSG_ERROR="${RED}[ERROR] 💥"
+MSG_INFO="${BLUE}[INFO] ℹ️ "
+MSG_OK="${GREEN}[OK] ✅"
+MSG_WARN="${YELLOW}[WARN] ⚠️ "
 
 MSG_END="${RESET}"
 
 ########
 
 function logger-debug {
-  printf '%b\n' "${MSG_DEBUG} ${2-👾}  ${1} ${MSG_END}"
+  printf '%b\n' "${MSG_DEBUG} ${1} ${MSG_END}"
 }
 export -f logger-debug
 
 function logger-docs {
-  printf '%b\n' "${MSG_DOCS} ${2-📝}  ${1} ${MSG_END}"
+  printf '%b\n' "${MSG_DOCS} ${1} ${MSG_END}"
 }
 export -f logger-docs
 
 function logger-error {
-  printf '%b\n' "${MSG_ERROR} ${2-💥}  ${1} ${MSG_END}"
+  printf '%b\n' "${MSG_ERROR} ${1} ${MSG_END}"
 }
 export -f logger-error
 
 function logger-info {
-  printf '%b\n' "${MSG_INFO} ${2-ℹ️}  ${1} ${MSG_END}"
+  printf '%b\n' "${MSG_INFO} ${1} ${MSG_END}"
 }
 export -f logger-info
 
 function logger-ok {
-  printf '%b\n' "${MSG_OK} ${2-✅}  ${1} ${MSG_END}"
+  printf '%b\n' "${MSG_OK} ${1} ${MSG_END}"
 }
 export -f logger-ok
 
 function logger-warn {
-  printf '%b\n' "${MSG_WARN} ${2-⚠️}  ${1} ${MSG_END}"
+  printf '%b\n' "${MSG_WARN} ${1} ${MSG_END}"
 }
 export -f logger-warn
