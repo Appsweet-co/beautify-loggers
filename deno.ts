61a7a34b95d1221c47d8310261fd83e0ff54f0b5
Derek Peruo <derek@derekperuo.net>
2021-10-29 08:25:00 -0500

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -1,9 +1,9 @@
 import { blue, green, red, yellow } from "https://deno.land/std@0.112.0/fmt/colors.ts";
 
-export const error = (...msgs: string[]) => console.log(red('==> [ERROR]'), ...msgs.map(red));
+export const error = (...msgs: string[]) => console.log(red('[ERROR]'), ...msgs.map(red));
 
-export const info = (...msgs: string[]) => console.log(blue('==> [INFO]'), ...msgs.map(blue));
+export const info = (...msgs: string[]) => console.log(blue('[INFO]'), ...msgs.map(blue));
 
-export const ok = (...msgs: string[]) => console.log(green('==> [OK]'), ...msgs.map(green));
+export const ok = (...msgs: string[]) => console.log(green('[OK]'), ...msgs.map(green));
 
-export const warn = (...msgs: string[]) => console.log(yellow('==> [WARN]'), ...msgs.map(yellow));
+export const warn = (...msgs: string[]) => console.log(yellow('[WARN]'), ...msgs.map(yellow));
