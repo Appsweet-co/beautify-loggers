bef2c80530c8a0553dd792d388afdb9350d848fd
Derek Peruo <derek@derekperuo.net>
2021-10-29 08:56:16 -0500

    feat: (deno.ts) Add new log options

--- deno.ts
+++ deno.ts
@@ -1,9 +1,17 @@
-import { blue, green, red, yellow } from "https://deno.land/std@0.112.0/fmt/colors.ts";
+import { blue, cyan, green, magenta, red, yellow } from "https://deno.land/std@0.112.0/fmt/colors.ts";
 
-export const error = (...msgs: string[]) => console.log(red('[ERROR]'), ...msgs.map(red));
+const prefix = (text: string) => `[${text.toUpperCase()}]`;
 
-export const info = (...msgs: string[]) => console.log(blue('[INFO]'), ...msgs.map(blue));
+export const dev = (...msgs: string[]) => console.log(magenta(prefix('dev')), ...msgs.map(magenta));
 
-export const ok = (...msgs: string[]) => console.log(green('[OK]'), ...msgs.map(green));
+export const docs = (...msgs: string[]) => console.log(cyan(prefix('docs')), ...msgs.map(cyan));
 
-export const warn = (...msgs: string[]) => console.log(yellow('[WARN]'), ...msgs.map(yellow));
+export const error = (...msgs: string[]) => console.log(red(prefix('error')), ...msgs.map(red));
+
+export const info = (...msgs: string[]) => console.log(blue(prefix('info')), ...msgs.map(blue));
+
+export const ok = (...msgs: string[]) => console.log(green(prefix('ok')), ...msgs.map(green));
+
+export const warn = (...msgs: string[]) => console.log(yellow(prefix('warn')), ...msgs.map(yellow));
+
+export const done = () => ok('Done!');
