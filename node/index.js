6c5804355918409b400e80b79a66b3b285aafeb2
dperuo <derek@derekperuo.net>
2021-10-29 12:48:35 -0500

    refactor: (index.js) Sort exports

--- node/index.js
+++ node/index.js
@@ -4,15 +4,15 @@ const { blue, cyan, green, magenta, red, yellow } = require('chalk');
 
 const prefix = (text) => `==> [${text.toUpperCase()}]`;
 
+const dev = (...msg) => console.log(magenta(prefix('dev'), '👾', ...msg));
 const docs = (...msg) => console.warn(cyan(prefix('docs'), '📝', ...msg));
 const error = (...msg) => console.error(red(prefix('error'), '💥', ...msg));
 const info = (...msg) => console.log(blue(prefix('info'), 'ℹ️ ', ...msg));
 const ok = (...msg) => console.log(green(prefix('ok'), '✅', ...msg));
 const warn = (...msg) => console.warn(yellow(prefix('warn'), '⚠️ ', ...msg));
-const dev = (...msg) => console.log(magenta(prefix('dev'), '👾', ...msg));
 
 const done = () => ok('Done!');
 
 module.exports = {
   dev, docs, done, error, info, ok, warn
-}
+};
