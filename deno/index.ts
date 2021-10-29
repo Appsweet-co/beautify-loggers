a9884e7ed06c333084ee4d59e8a8406e86119916
Derek Peruo <derek@derekperuo.net>
2021-10-29 09:02:52 -0500

    Create index.ts

---
+++ deno/index.ts
@@ -0,0 +1,17 @@
+import { blue, cyan, green, magenta, red, yellow } from "https://deno.land/std@0.112.0/fmt/colors.ts";
+
+const prefix = (text: string) => `[${text.toUpperCase()}]`;
+
+export const dev = (...msgs: string[]) => console.log(magenta(prefix('dev')), ...msgs.map(magenta));
+
+export const docs = (...msgs: string[]) => console.log(cyan(prefix('docs')), ...msgs.map(cyan));
+
+export const error = (...msgs: string[]) => console.log(red(prefix('error')), ...msgs.map(red));
+
+export const info = (...msgs: string[]) => console.log(blue(prefix('info')), ...msgs.map(blue));
+
+export const ok = (...msgs: string[]) => console.log(green(prefix('ok')), ...msgs.map(green));
+
+export const warn = (...msgs: string[]) => console.log(yellow(prefix('warn')), ...msgs.map(yellow));
+
+export const done = () => ok('Done!');
